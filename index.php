274b59f3c7d8f2043bc49d41ea249650e46d4984
undannielmas <daniel@markalia.mx>
2022-05-24 16:42:47 -0500

    agregar campos al beneficiario

--- index.php
+++ index.php
@@ -174,8 +174,8 @@
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <label for="nacionalidad-beneficiario">Nacionalidad:</label>
-                        <select class="custom-select mr-sm-2" name="nacionalidad-beneficiario" id="nacionalidad">
+                        <label for="nacionalidad">Nacionalidad:</label>
+                        <select class="custom-select mr-sm-2" name="nacionalidad" id="nacionalidad">
                             <option selected>Elegir respuesta...</option>
                             <option value="Mexicano">Mexicano</option>
                             <option value="Mexicana">Mexicana</option>
@@ -184,16 +184,65 @@
                         </select>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="nacimiento-beneficiario">Lugar De Nacimiento:</label>
-                        <input type="text" class="form-control" name="nacimiento-beneficiario"
-                            id="nacimiento-beneficiario" placeholder="Monterrey, Nuevo León" minlength="4"
-                            maxlength="14" size="10">
+                        <label for="nacionalidad">Estado de Residencia:</label>
+                        <select class="custom-select mr-sm-2" name="estado" id="estado">
+                            <option selected>Elegir respuesta...</option>
+                            <option value="Aguascalientes">Aguascalientes</option>
+                            <option value="Baja California">Baja California</option>
+                            <option value="Baja California Sur">Baja California Sur</option>
+                            <option value="Campeche">Campeche</option>
+                            <option value="Chiapas">Chiapas</option>
+                            <option value="Chihuahua">Chihuahua</option>
+                            <option value="CDMX">Ciudad de México</option>
+                            <option value="Coahuila">Coahuila</option>
+                            <option value="Colima">Colima</option>
+                            <option value="Durango">Durango</option>
+                            <option value="Estado de México">Estado de México</option>
+                            <option value="Guanajuato">Guanajuato</option>
+                            <option value="Guerrero">Guerrero</option>
+                            <option value="Hidalgo">Hidalgo</option>
+                            <option value="Jalisco">Jalisco</option>
+                            <option value="Michoacán">Michoacán</option>
+                            <option value="Morelos">Morelos</option>
+                            <option value="Nayarit">Nayarit</option>
+                            <option value="Nuevo León">Nuevo León</option>
+                            <option value="Oaxaca">Oaxaca</option>
+                            <option value="Puebla">Puebla</option>
+                            <option value="Querétaro">Querétaro</option>
+                            <option value="Quintana Roo">Quintana Roo</option>
+                            <option value="San Luis Potosí">San Luis Potosí</option>
+                            <option value="Sinaloa">Sinaloa</option>
+                            <option value="Sonora">Sonora</option>
+                            <option value="Tabasco">Tabasco</option>
+                            <option value="Tamaulipas">Tamaulipas</option>
+                            <option value="Tlaxcala">Tlaxcala</option>
+                            <option value="Veracruz">Veracruz</option>
+                            <option value="Yucatán">Yucatán</option>
+                            <option value="Zacatecas">Zacatecas</option>
+                        </select>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="home-beneficiario">Domicilio (Incluyendo C.P.):</label>
-                    <input type="text" class="form-control" name="home-beneficiario" id="home-beneficiario"
-                        placeholder="Calle, Numero, Colonia, C.P">
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="calle">Calle:</label>
+                        <input type="text" class="form-control" name="calle" id="calle"
+                            placeholder="Calle..." minlength="4" maxlength="30" size="10">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="numero">Numero:</label>
+                        <input type="text" class="form-control" name="numero" id="numero" placeholder="Numero..."
+                            minlength="4" maxlength="14" size="10">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="colonia">Colonia:</label>
+                        <input type="text" class="form-control" name="colonia" id="colonia" placeholder="Colonia..."
+                            minlength="4" maxlength="50" size="10">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="rfc">Codigo Postal</label>
+                        <input type="text" class="form-control" name="cp" id="cp" placeholder="C.P"
+                            minlength="1" maxlength="5" size="10" style="text-transform:uppercase">
+                    </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
